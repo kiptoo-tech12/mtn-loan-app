88b590f9fe1fddac778a473f6249c3fc11cdc894
kiptoo-tech12 <ghostkip755@gmail.com>
2026-05-30 10:58:29 +0300

    Create App.jsx

---
+++ server/client/src/client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>MTN Loan App</h1>
+      <p>Application deployed successfully.</p>
+    </div>
+  );
+}
+
+export default App;
